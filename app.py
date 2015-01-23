c2fe222cc24614955cbfca0ce517e72238684390
kevmo <k.james.moore@gmail.com>
2015-01-23 17:21:50 -0600

    WIP: Just finished URL map

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import urlparse
 
 import redis
+# Request wraps the environ for easier access to the request variables (form data, headers)
+# Response is a standard WSGI app.
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
@@ -14,10 +16,23 @@ class Shortly(object):
     """The WSGI application object."""
     def __init__(self, config):
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        self.jinja_env = Environment(loader=FileSystemLoader(template_path),
+                                     autoescape=True)
+        self.url_map = Map([
+            Rule('/', endpoint='new_url'),
+            Rule('/<short_id>', endpoint='follow_short_link'),
+            Rule('/<short_id>+', endpoint='short_link_details')
+        ])
+
+    def render_template(self, template_name, **context):
+        t = self.jinja_env.get_template(template_name)
+        return Response(t.render(context), mimetype='text/html')
 
     def dispatch_request(self, request):
         return Response("Haro kittay")
 
+    # The actual application to be returned when an instance of Shortly is called.
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
         response = self.dispatch_request(request)
@@ -28,11 +43,15 @@ class Shortly(object):
 
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+    """Application factory."""
+
+    # Configure your WSGI application object.
     app = Shortly({
         'redis_host': redis_host,
         'redis_port': redis_port
     })
 
+    # Attach static file path configuration to the application object
     if with_static:
         app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
             '/static': os.path.join(os.path.dirname(__file__), 'static')
@@ -40,6 +59,7 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
 
     return app
 
+# FIRE ZE MISSILE
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = create_app()
